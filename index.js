d91d7cadc300fc8d27b8582435ab1204582f6125
Pietrek1989 <68666992+Pietrek1989@users.noreply.github.com>
2022-11-29 16:57:34 +0100

    Fixed excersive 8

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 //Trials
 
 
+let isInteger = "2";
+if (typeof isInteger === "number") {
+    console.log("Yes the variable is integer")
+} else {
+    console.log("No, the value is not an integer")
+}
